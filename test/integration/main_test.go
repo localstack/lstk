d92eb9ccb1b4fee73434be40c294c8cc58c2b8bb
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2026-02-04 17:00:31 +0100

    Add Unit/Integration testing jobs to CI pipeline (#13)

--- test/integration/main_test.go
+++ test/integration/main_test.go
@@ -1,18 +1,28 @@
 package integration_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/docker/docker/client"
 )
 
 var dockerClient *client.Client
+var dockerAvailable bool
 
 func TestMain(m *testing.M) {
 	var err error
 	dockerClient, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
+	if err == nil {
+		_, err = dockerClient.Ping(context.Background())
+		dockerAvailable = err == nil
 	}
 	m.Run()
+}
+
+func requireDocker(t *testing.T) {
+	t.Helper()
+	if !dockerAvailable {
+		t.Skip("Docker is not available")
+	}
 }
