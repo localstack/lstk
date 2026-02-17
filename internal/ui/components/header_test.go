e806d638423a267ad6d01f3ef1a7204d43bd4799
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2026-02-17 15:45:32 +0100

    Make header test idempotent

--- internal/ui/components/header_test.go
+++ internal/ui/components/header_test.go
@@ -9,7 +9,9 @@ import (
 )
 
 func TestHeaderView(t *testing.T) {
+	original := lipgloss.ColorProfile()
 	lipgloss.SetColorProfile(termenv.TrueColor)
+	t.Cleanup(func() { lipgloss.SetColorProfile(original) })
 
 	view := NewHeader("v1.0.0").View()
 	golden.Assert(t, view, "header.golden")
