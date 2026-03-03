f75e8f159132444fea6cdbeca28cd5a6b995f588
George Tsiolis <tsiolis.g@gmail.com>
2026-03-04 01:10:07 +0200

    Migrate stop command to output event system

--- internal/container/stop.go
+++ internal/container/stop.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/containerd/errdefs"
 	"github.com/localstack/lstk/internal/config"
+	"github.com/localstack/lstk/internal/output"
 	"github.com/localstack/lstk/internal/runtime"
 )
 
-func Stop(ctx context.Context, rt runtime.Runtime, onProgress func(string)) error {
+func Stop(ctx context.Context, rt runtime.Runtime, sink output.Sink) error {
 	cfg, err := config.Get()
 	if err != nil {
 		return fmt.Errorf("failed to get config: %w", err)
@@ -17,14 +17,17 @@ func Stop(ctx context.Context, rt runtime.Runtime, onProgress func(string)) erro
 
 	for _, c := range cfg.Containers {
 		name := c.Name()
-		onProgress(fmt.Sprintf("Stopping %s...", name))
+		running, err := rt.IsRunning(ctx, name)
+		if err != nil || !running {
+			return fmt.Errorf("%s is not running", name)
+		}
+		output.EmitSpinnerStart(sink, fmt.Sprintf("Stopping %s", name))
 		if err := rt.Stop(ctx, name); err != nil {
-			if errdefs.IsNotFound(err) {
-				return fmt.Errorf("%s is not running", name)
-			}
+			output.EmitSpinnerStop(sink)
 			return fmt.Errorf("failed to stop %s: %w", name, err)
 		}
-		onProgress(fmt.Sprintf("%s stopped", name))
+		output.EmitSpinnerStop(sink)
+		output.EmitSuccess(sink, fmt.Sprintf("%s stopped", name))
 	}
 
 	return nil
---
+++ internal/ui/run_stop.go
@@ -0,0 +1,47 @@
+package ui
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/localstack/lstk/internal/container"
+	"github.com/localstack/lstk/internal/output"
+	"github.com/localstack/lstk/internal/runtime"
+)
+
+func RunStop(parentCtx context.Context, rt runtime.Runtime) error {
+	_, cancel := context.WithCancel(parentCtx)
+	defer cancel()
+
+	app := NewApp("", "", "", cancel)
+	p := tea.NewProgram(app, tea.WithInput(os.Stdin), tea.WithOutput(os.Stdout))
+	runErrCh := make(chan error, 1)
+
+	go func() {
+		err := container.Stop(parentCtx, rt, output.NewTUISink(programSender{p: p}))
+		runErrCh <- err
+		if err != nil && !errors.Is(err, context.Canceled) {
+			p.Send(runErrMsg{err: err})
+			return
+		}
+		p.Send(runDoneMsg{})
+	}()
+
+	model, err := p.Run()
+	if err != nil {
+		return err
+	}
+
+	if app, ok := model.(App); ok && app.Err() != nil {
+		return app.Err()
+	}
+
+	runErr := <-runErrCh
+	if runErr != nil && !errors.Is(runErr, context.Canceled) {
+		return runErr
+	}
+
+	return nil
+}
