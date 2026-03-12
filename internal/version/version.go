37b701e0984ad8f735d4c5e7a3f4733fc0e426ed
George Tsiolis <tsiolis.g@gmail.com>
2026-03-12 21:01:37 +0200

    Hide build metadata from public version API

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,6 +8,4 @@ var (
 	buildDate = "unknown"
 )
 
-func Version() string   { return version }
-func Commit() string    { return commit }
-func BuildDate() string { return buildDate }
+func Version() string { return version }
