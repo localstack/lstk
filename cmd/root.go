416873414b7d435c9643a450b2c3d54961f62dfc
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2026-03-23 15:47:57 +0100

    Add hidden docs command for man page and markdown generation

--- cmd/root.go
+++ cmd/root.go
@@ -61,6 +61,7 @@ func NewRootCmd(cfg *env.Env, tel *telemetry.Client, logger log.Logger) *cobra.C
 		newLogsCmd(cfg, tel),
 		newConfigCmd(cfg, tel),
 		newUpdateCmd(cfg, tel),
+		newDocsCmd(),
 	)
 
 	return root
