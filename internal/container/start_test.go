9ee6c8e80e1cca1d8a0f8486f5d0dbfe843940a1
carole-lavillonniere <carole.lavillonniere@gmail.com>
2026-04-14 14:34:08 +0200

    fix(lint): check error return from ln.Close in test

--- internal/container/start_test.go
+++ internal/container/start_test.go
@@ -117,7 +117,7 @@ func TestSelectContainersToStart_QueuesContainerWhenNoneRunningOnPort(t *testing
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 	freePort := ln.Addr().(*net.TCPAddr).Port
-	ln.Close()
+	require.NoError(t, ln.Close())
 
 	c := runtime.ContainerConfig{
 		Image:         "localstack/localstack-pro:3.5.0",
